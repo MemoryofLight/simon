aee61e8ba68dffd81d6b5841e77df4ecf0915026
Alex Walbom <alexwalbom@gmail.com>
2023-03-04 00:20:25 -0700

    finished play.js

--- play.js
+++ play.js
@@ -17,6 +17,20 @@ const btnDescriptions = [
         const background = `hsl(${this.hue}, 100%, ${level}%)`;
         this.el.style.backgroundcolor = background;
     }
+
+    async press(volume) {
+        this.paint(50);
+        await this.play(volume);
+        this.paint(25);
+    }
+
+    async play(volume = 1.0) {
+        this.sound.volume = volume;
+        await new Promise((resolve) => {
+            this.sound.onended = resolve;
+            this.sound.play();
+        })
+    }
   }
 
   class Game {
@@ -46,10 +60,119 @@ const btnDescriptions = [
     getPlayerName() {
         return localStorage.getItem('username') ?? 'Mystery Player';
     }
+
+    async pressButton(button) {
+        if(this.allowPlayer) {
+            this.allowPlayer = false;
+            await this.buttons.get(button.id).press(1.0);
+
+            if(this.sequence[this.playerPlaybackPos].el.id === button.id) {
+                this.playerPlaybackPos++;
+                if(this.playerPlaybackPos === this.sequence.length) {
+                    this.playerPlaybackPos = 0;
+                    this.addButton();
+                    this.updateScore(this.sequence.length - 1);
+                    await this.playSequence();
+                }
+                this.allowPlayer = true;
+            }
+            else {
+                this.saveScore(this.sequence.length - 1);
+                this.mistakeSound.play();
+                await this.buttonDance(3);
+            }
+        }
+    }
+
+    async reset() {
+        this.allowPlayer = false;
+        this.playerPlaybackPos = 0;
+        this.sequence = [];
+        this.updateScore('---');
+        await this.buttonDance(1);
+        this.addButton();
+        await this.playSequence();
+        this.allowPlayer = true;
+    }
+
+    updateScore(score) {
+        const scoreEl = document.querySelector('#score');
+        scoreEl.textContent = score;
+    }
+
+    async buttonDance(laps = 1) {
+        for(let step = 0; step < laps; step++) {
+            for(const btn of this.buttons.values()) {
+                await btn.press(0.0);
+            }
+        }
+    }
+
+    addButton() {
+        const btn = this.getRandomButton();
+        this.sequence.push(btn);
+    }
+
+    getRandomButton() {
+        let buttons = Array.from(this.buttons.values());
+        return buttons[Math.floor(Math.random() * this.buttons.size)];
+    }
+
+    async playSequence() {
+        await delay(500);
+        for(const btn of this.sequence) {
+            await btn.press(1.0);
+            await delay(100);
+        }
+    }
+
+    saveScore(score) {
+        const userName = this.getPlayerName();
+        let scores = [];
+        const scoresText = localStorage.getitem('scores');
+        if(scoresText) {
+            scores = JSON.parse(scoresText);
+        }
+        scores = this.updateScores(userName, score, scores);
+
+        localStorage.setItem('scores', JSON.stringify(scores));
+    }
+
+    updateScores(userName, score, scores) {
+        const date = new Date().toLocaleDateString();
+        const newScore = {name: userName, score: score, date: date};
+
+        let found = false;
+        for (const [i, prevScore] of scores.entries()) {
+            if (score > prevScore.score) {
+                scores.splice(i, 0, newScore);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            scores.push(newScore);
+        }
+
+        if (scores.length > 10) {
+            scores.length = 10;
+        }
+
+        return scores;
+    }
   }
 
   const game = new Game();
 
   function loadSound(filename) {
     return new Audio('assets/' + filename);
+  }
+
+  function delay(milliseconds) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(true);
+        }, milliseconds);
+    });
   }
